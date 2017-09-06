91973776f8358be5624e7d85fc07e03b59c942e9
Ankita-Palekar <453ankitapalekar@gmail.com>
2017-09-06 16:10:20 +0530

    initial set up commit

---
+++ bucketlist/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
---
+++ pyvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/scoders/timepass/python/bucketlist/pyvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
